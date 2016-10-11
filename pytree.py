80cdbfae2f7196bce63a83a4be4e279695e35926
elyapardes <elyapardes@gmail.com>
2016-10-10 23:17:00 -0400

    Update pytree.py

--- pytree.py
+++ pytree.py
@@ -5,66 +5,70 @@ import os
 from os import listdir, sep, walk
 from os.path import basename, isdir
 import re
-
 import string
 import sys
 
-def count_file(files, num_file):
-    totalfile = []
-    for f in files:
-        if not f.startswith('.'):
-            totalfile.append(f)
-    nfile = len(totalfile)
-    return nfile
+def printDir(path, padding, isLast):
+    if isdir(path):
+        if isLast:
+            padding = padding + '    '
+            tree(path, padding, isLast=False)
+        else:
+            padding = padding + '│   '
+            tree(path, padding, isLast=False)
 
-def tree(dir, spacing, flaglast=False):
+
+def tree(dir, padding, isLast=False):
     files = []
     files = [files for files in listdir(dir) if not files.startswith('.')]
     allfiles = sorted(files, key=lambda x: x.lower())
     for i, filename in enumerate(allfiles):
         path = dir + sep + filename
         if (i == len(files) - 1):
-            flaglast = True
-            print(spacing + '└── ' + filename)
+            isLast = True
+            print(padding + '└── ' + filename)
         else:
-            flaglast = False
-            print(spacing + '├── ' + filename)
-        printDir(path, spacing, flaglast)
-    spacing = spacing + '    '
+            isLast = False
+            print(padding + '├── ' + filename)
+        printDir(path, padding, isLast)
+    padding = padding + '    '
 
-    def printDir(path, spacing, flaglast):
-    if isdir(path):
-        if flaglast:
-            spacing = spacing + '    '
-            tree(path, spacing, flaglast=False)
-        else:
-            spacing = spacing + '│   '
-            tree(path, spacing, flaglast=False)
 
+# function to track number of directories and files in given path
 def fileTrack(path):
-    ndir = 0
-    nfiles = 0
+    num_dir = 0
+    num_files = 0
     for path, dirs, files in walk(path):
+        # Reference: http://stackoverflow.com/questions/13454164/os-walk-without-hidden-folders
         dirs[:] = [d for d in dirs if not d.startswith('.')]
-        ndir = ndir + len(dirs)
-        nfile = count_file(files, nfiles)
-        nfiles += nfile
-    print("%s directories, %s files" % (ndir, nfiles))
+        num_dir = num_dir + len(dirs)
+        num_file = count_file(files, num_files)
+        num_files += num_file
+    print("%s directories, %s files" % (num_dir, num_files))
 
-# Code was written with the help of classmate, and using stackoverflow
-    
+
+# function to fix complexity issue by codeclimate
+def count_file(files, num_file):
+    total_file = []
+    for f in files:
+        if not f.startswith('.'):
+            total_file.append(f)
+    num_file = len(total_file)
+    return num_file
 
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
         print('.')
         path = os.getcwd()
-        no_files = tree(path, '', flaglast=False)
+        no_files = tree(path, '', isLast=False)
         print('')
         fileTrack(path)
     elif len(sys.argv) == 2:
         print(sys.argv[1])
         path = sys.argv[1]
-        no_files = tree(path, '', flaglast=False)
+        no_files = tree(path, '', isLast=False)
         print('')
         fileTrack(path)
+    else:
+        print('Please enter with only one path.')
