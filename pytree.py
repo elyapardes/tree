14c03f49abdb45551bf996a11387badc39324a85
elyapardes <elyapardes@gmail.com>
2016-10-10 23:30:33 -0400

    Update pytree.py

--- pytree.py
+++ pytree.py
@@ -20,10 +20,7 @@ def printDir(path, padding, isLast):
 
 def tree(dir, padding, isLast=False):
     files = []
-# Reference: http://stackoverflow.com/questions/7099290/how-to-ignore-hidden-files-using-os-listdir
     files = [files for files in listdir(dir) if not files.startswith('.')]
-# Reference: http://stackoverflow.com/questions/13589560/how-to-sort-list-of-string-without-considering-special-characters-and-with-case
-# allfiles = sorted(files, key=lambda x: re.sub('[^A-Za-z]+', '', x).lower())
     allfiles = sorted(files, key=lambda x: x.lower())
     for i, filename in enumerate(allfiles):
         path = dir + sep + filename
@@ -37,20 +34,16 @@ def tree(dir, padding, isLast=False):
     padding = padding + '    '
 
 
-# function to track number of directories and files in given path
 def fileTrack(path):
     num_dir = 0
     num_files = 0
     for path, dirs, files in walk(path):
-        # Reference: http://stackoverflow.com/questions/13454164/os-walk-without-hidden-folders
         dirs[:] = [d for d in dirs if not d.startswith('.')]
         num_dir = num_dir + len(dirs)
         num_file = count_file(files, num_files)
         num_files += num_file
     print("%s directories, %s files" % (num_dir, num_files))
 
-
-# function to fix complexity issue by codeclimate
 def count_file(files, num_file):
     total_file = []
     for f in files:
@@ -59,6 +52,7 @@ def count_file(files, num_file):
     num_file = len(total_file)
     return num_file
 
+# Code written with the help of classmates
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
